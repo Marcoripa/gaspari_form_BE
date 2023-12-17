aa7535ff4434d6ec7e51f46452484c5f50e7f5f1
Marcoripa <marcoripa@hotmail.it>
2023-12-17 18:46:22 +0100

    develop: - set up form and style

--- resources/js/components/UserForm.vue
+++ resources/js/components/UserForm.vue
@@ -1,7 +1,56 @@
 <script setup>
-    console.log('ciao')
+console.log("ciao");
 </script>
 
 <template>
-    <h1>Hello</h1>
+    <div id="userform-container">
+        <form action="/action_page.php" method="post">
+            <div class="container">
+                <div class="input-row">
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        name="firstName"
+                        required
+                    />
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        name="lastName"
+                        required
+                    />
+                </div>
+                <input type="email" placeholder="Email" name="email" required />
+
+                <div class="input-row">
+                    <label for="country"><b>Select your Country</b></label>
+                    <select name="country" id="country">
+                        <option value="italy">Italy</option>
+                        <option value="france">France</option>
+                        <option value="australia">Australia</option>
+                        <option value="usa">USA</option>
+                    </select>
+
+                    <label for="legalAge"><b>Are you older than 21?</b></label>
+                    <input type="checkbox" name="legalAge" required />
+                </div>
+
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="psw"
+                    required
+                />
+
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="pswRepeat"
+                    required
+                />
+
+                <button type="submit">Register New User</button>
+            </div>
+        </form>
+    </div>
 </template>
